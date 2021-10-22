2a863a68064c8e562170e61e514b0f80bff2c9de
rickyalmeida <ricky.almeida@rxpro.com.br>
2021-10-22 18:57:32 -0300

    feat: 🎸 add onPress functionality to TodoCard with tests

--- source/components/TodoCard/TodoCard.js
+++ source/components/TodoCard/TodoCard.js
@@ -2,9 +2,10 @@ import React from 'react'
 import {Text, View, TouchableOpacity} from 'react-native'
 import tw from 'tailwind-react-native-classnames'
 
-const TodoCard = ({children}) => (
+const TodoCard = ({children, index, onPress}) => (
   <TouchableOpacity
     style={tw`w-full h-14 flex-row items-center p-4 bg-white rounded-lg border border-gray-100 shadow-md`}
+    onPress={() => onPress(index)}
   >
     <View style={tw`w-6 h-6 bg-blue-200 rounded-md`} />
     <Text style={tw`flex-1 ml-4`}>{children}</Text>
--- source/components/TodoCard/TodoCard.test.js
+++ source/components/TodoCard/TodoCard.test.js
@@ -1,9 +1,18 @@
 import React from 'react'
-import {render} from '@testing-library/react-native'
+import {fireEvent, render} from '@testing-library/react-native'
 import TodoCard from './TodoCard'
 
+const makeProps = props => ({children: 'foo bar', index: 0, onPress: jest.fn(), ...props})
+
 it('should render the todo name', () => {
-  const todo = 'foo bar'
-  const screen = render(<TodoCard>{todo}</TodoCard>)
-  expect(screen.getByText(todo)).toBeTruthy()
+  const props = makeProps()
+  const screen = render(<TodoCard {...props} />)
+  expect(screen.getByText(props.children)).toBeTruthy()
+})
+
+it('should call the given callback with the todo index on press', () => {
+  const props = makeProps()
+  const screen = render(<TodoCard {...props} />)
+  fireEvent.press(screen.getByText(props.children))
+  expect(props.onPress).toHaveBeenCalledWith(0)
 })
