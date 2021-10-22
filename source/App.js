76561a9f4ff1c3c4d8014d0658c1f2eed95bbc4b
rickyalmeida <ricky.almeida@rxpro.com.br>
2021-10-22 20:37:00 -0300

    feat: 🎸 add functionalties to create and remove todos

--- source/App.js
+++ source/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {
   FlatList,
   Keyboard,
@@ -9,27 +9,54 @@ import {
 } from 'react-native'
 import {StatusBar} from 'expo-status-bar'
 import tw from 'tailwind-react-native-classnames'
-import {CreateTodoField, TodoCard} from './components'
+import {CreateTodoForm, TodoCard} from './components'
 
-const todos = ['Like 👍', 'Comment 📢', 'Subscribe 🙃']
+const App = () => {
+  const [todos, setTodos] = useState(['some'])
+  const [input, setInput] = useState('')
 
-const App = () => (
-  <>
-    <StatusBar style="auto" />
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <SafeAreaView style={tw`bg-gray-200 flex-1`}>
-        <Text style={tw`ml-5 mt-10 mb-5 font-bold text-2xl`}>Today’s tasks</Text>
-        <FlatList
-          contentContainerStyle={tw`p-5`}
-          data={todos}
-          keyExtractor={todo => todo}
-          ItemSeparatorComponent={() => <View style={tw`h-5`} />}
-          renderItem={({item: todo}) => <TodoCard>{todo}</TodoCard>}
-        />
-        <CreateTodoField />
-      </SafeAreaView>
-    </TouchableWithoutFeedback>
-  </>
-)
+  const handleCreateTodo = () => {
+    if (!input.trim()) {
+      return
+    }
+
+    setTodos(prevState => [input, ...prevState])
+    setInput('')
+    Keyboard.dismiss()
+  }
+
+  const handleDeleteTodo = index => {
+    const copy = [...todos]
+    copy.splice(index, 1)
+    setTodos(copy)
+  }
+
+  return (
+    <>
+      <StatusBar style="auto" />
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <SafeAreaView style={tw`bg-gray-200 flex-1`}>
+          <Text style={tw`ml-5 mt-10 mb-5 font-bold text-2xl`} accessibilityRole="header">
+            Today's tasks
+          </Text>
+          <FlatList
+            contentContainerStyle={tw`p-5`}
+            data={todos}
+            keyExtractor={todo => todo}
+            ItemSeparatorComponent={() => <View style={tw`h-5`} />}
+            renderItem={({item: todo}) => (
+              <TodoCard onPress={handleDeleteTodo}>{todo}</TodoCard>
+            )}
+          />
+          <CreateTodoForm
+            onChangeText={setInput}
+            value={input}
+            onSubmit={handleCreateTodo}
+          />
+        </SafeAreaView>
+      </TouchableWithoutFeedback>
+    </>
+  )
+}
 
 export default App
